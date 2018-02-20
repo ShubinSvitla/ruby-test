98cc64508eb7a94ff178816c703abf19a59df3e1
Sergey Shubin <serg.shubin@gmail.com>
2018-02-20 23:40:56 +0200

    BEGIN END bloks

--- test.rb
+++ test.rb
@@ -1,23 +1,12 @@
-$x = 5;
+print "Hello world!";
 
-print <<HEREDOC
-  This is the first way of creating
-  here document ie. multiple line string.
-  One plus one is #{1 + 1} or #{$x}?
-****************
-HEREDOC
+END {
+  print "End of the program.";
+}
 
-print <<"HEREDOC" # same as above
-  This is the second way of creating
-  here document ie. multiple line string.
-  One plus one is #{1 + 1} or #{$x}?
-****************
-HEREDOC
-
-print <<`HEREDOC` # execute commands
-  echo Hi there
-HEREDOC
-
+BEGIN {
+  print "Start of the program.";
+}
 
 
 
